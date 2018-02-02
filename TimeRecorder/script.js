c0cda39d5a4a6df77dd72e1230d5035808519c16
idunnourname <36059591+idunnourname@users.noreply.github.com>
2018-02-02 15:14:56 +0900

    Update script.js

--- TimeRecorder/script.js
+++ TimeRecorder/script.js
@@ -1,1 +1,8 @@
-console.log("test");
+function record(){
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  var time = h + ":" + m + ":" + s;
+  localStorage(time, "Recorded");
+}
